267e1c4b30ab2cbd83b195d08c31ae462933d8ad
Sushmita221998 <sushmitaghatge22@gmail.com>
2026-03-10 16:44:06 +0530

    Added footer-new

---
+++ blocks/footer-new/footer-new.js
@@ -0,0 +1,34 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+/**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+function renderImage(block){
+const link = block.querySelector('a');
+  if (link) {
+    const imageUrl = link.href;
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.alt = link.textContent;
+    img.loading = 'lazy';
+    block.textContent = '';
+    block.appendChild(img);
+}
+}
+
+export default async function decorate(block) {
+  // load footer as fragment
+  const footerMeta = getMetadata('footer');
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const fragment = await loadFragment(footerPath);
+
+  // decorate footer DOM
+  block.textContent = '';
+  const footer = document.createElement('div');
+  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  renderImage(block);
+  block.append(footer);
+}
