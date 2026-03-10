ce694890e1e81617d96a7014009debd317217a4c
Sushmita221998 <sushmitaghatge22@gmail.com>
2026-03-10 16:49:08 +0530

    Updated file

--- blocks/footer-new/footer-new.js
+++ blocks/footer-new/footer-new.js
@@ -29,6 +29,7 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
-  renderImage(block);
   block.append(footer);
+
+  renderImage(block);
 }
