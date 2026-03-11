3020b1e482d34cb7668fc2e83b671ce22dc2f426
Sushmita221998 <sushmitaghatge22@gmail.com>
2026-03-11 13:59:00 +0530

    updated footer-table

--- blocks/footer-table/footer-table.js
+++ blocks/footer-table/footer-table.js
@@ -1,10 +1,10 @@
 export default function decorate(block) {
   // Render the external image
   const cells = block.querySelectorAll('div > div');
-  cells.forEach(function (cell) {
+  cells.forEach((cell) => {
     const links = cell.querySelectorAll('a[href$=".jpg"],a[href$=".jpeg"],a[href$=".png"]');
 
-    links.forEach(function (link) {
+    links.forEach((link) => {
       const img = document.createElement('img');
       img.src = link.href;
       img.alt = '';
