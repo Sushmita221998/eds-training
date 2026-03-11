7a11b43b4137c743d79a77923f435067d13cef65
Sushmita221998 <sushmitaghatge22@gmail.com>
2026-03-11 13:28:23 +0530

    Update footer-table

--- blocks/footer-table/footer-table.js
+++ blocks/footer-table/footer-table.js
@@ -1,14 +1,15 @@
 export default function decorate(block) {
-  const cells=block.querySelectorAll('div > div');
-  cells.forEach(function (cell){
+  //Render the external image
+  const cells = block.querySelectorAll('div > div');
+  cells.forEach(function (cell) {
     const links = cell.querySelectorAll('a[href$=".jpg"],a[href$=".jpeg"],a[href$=".png"]');
 
-    links.forEach(function (link){
-    const img = document.createElement('img');
-    img.src = link.href;
-    img.alt = '';
-    img.loading = 'lazy';
-    link.replaceWith(img);
-  });
+    links.forEach(function (link) {
+      const img = document.createElement('img');
+      img.src = link.href;
+      img.alt = '';
+      img.loading = 'lazy';
+      link.replaceWith(img);
+    });
   });
 }
