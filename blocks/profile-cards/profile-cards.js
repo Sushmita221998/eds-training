2ff0b05fe94f7b496ec3867b1154b9e56166a92f
Sushmita221998 <sushmitaghatge22@gmail.com>
2026-03-18 19:34:47 +0530

    Added new css and js

---
+++ blocks/profile-cards/profile-cards.js
@@ -0,0 +1,34 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+//New Function Added
+function showHideProfileCards() {
+  document.querySelectorAll('.profile-cards ul li').forEach((card) => {
+    card.addEventListener('mouseenter', () => {
+      card.querySelector('.cards-card-image').style.display = 'none';
+      card.querySelector('.cards-card-body').style.display = 'block';
+    })
+    card.addEventListener('mouseleave', () => {
+      card.querySelector('.cards-card-image').style.display = 'block';
+      card.querySelector('.cards-card-body').style.display = 'none';
+    })
+  });
+}
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  block.textContent = '';
+  block.append(ul);
+  showHideProfileCards();
+
+}
